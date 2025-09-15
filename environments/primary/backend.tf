9e27a8c86f9bd1d6e156690cfa509dc98d27de1d
Greg <greg.renfro93@gmail.com>
2025-09-15 10:36:20 -0700

    adding s3 bucket policies and replication

--- environments/primary/backend.tf
+++ environments/primary/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "pilot-dr-recovery-primary"
     key = "pilot-light-dr/primary/terraform.tfstate"
-    region = "us-east-1"
+    region = var.aws_region
     encrypt = true
     dynamodb_table = "terraform-state-locks"
   }
